40beb4dbd6616c78a82b06df3fe1d0e2e1692ae8
Manos Pappas <mano.pappa@gmail.com>
2022-02-16 21:15:26 +0000

    EM-02 Setup Server + Move csv under data

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const responseTime = require('response-time');
+
+// const pricesRouter = require('./routes/pricesRouter');
+// require('./cacheManager');
+
+const api = require('./routes/api');
+
+const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
+
+app.use(morgan('combined'));
+app.use(responseTime());
+
+app.use(express.json());
+
+app.use('/v1', api);
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the Birthday Wishes Service! Your friends will love you!');
+});
+
+module.exports = app;
