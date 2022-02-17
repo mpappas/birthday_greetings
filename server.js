96ec7bf358158b450caf78573af1bf2b0795b8bb
Manos Pappas <mano.pappa@gmail.com>
2022-02-17 22:38:23 +0000

    EM-03 Implement loadAllFriendsWithBirthday to load list of friends on server spinup

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const http = require('http');
 
 const app = require('./app');
+const { loadAllFriendsWithBirthday } = require('./models/bdays.model');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
 async function startServer() {
+  await loadAllFriendsWithBirthday();
+
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
