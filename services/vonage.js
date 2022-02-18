45ad799b1a5ab2ccd4aafa4b15209d4c6b9a1b7a
Manos Pappas <mano.pappa@gmail.com>
2022-02-18 02:26:18 +0000

    EM-08 Implement sms functionality

---
+++ services/vonage.js
@@ -0,0 +1,29 @@
+require('dotenv').config({ path: __dirname + '/../.env' });
+
+const VONAGE_API_KEY = process.env.VONAGE_API_KEY;
+const VONAGE_API_SECRET = process.env.VONAGE_API_SECRET;
+const TO_NUMBER = process.env.VONAGE_TO_NUMBER;
+const VONAGE_BRAND_NAME = process.env.VONAGE_BRAND_NAME;
+
+const Vonage = require('@vonage/server-sdk');
+
+const vonage = new Vonage({
+  apiKey: VONAGE_API_KEY,
+  apiSecret: VONAGE_API_SECRET,
+});
+
+const from = VONAGE_BRAND_NAME;
+const to = TO_NUMBER;
+const text = 'A text message sent using the Vonage SMS API';
+
+vonage.message.sendSms(from, to, text, (err, responseData) => {
+  if (err) {
+    console.log(err);
+  } else {
+    if (responseData.messages[0]['status'] === '0') {
+      console.log('Message sent successfully.');
+    } else {
+      console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+    }
+  }
+});
